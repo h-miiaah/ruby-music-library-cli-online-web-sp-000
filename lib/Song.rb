ae80e35bcc51e0762b941bd46ebef71f900895f5
Haim D <haim11293@aol.com>
2019-11-13 15:55:08 +0000

    Automatically backed up by Learn

--- lib/Song.rb
+++ lib/Song.rb
@@ -47,7 +47,7 @@ class Song
   end
 
   def self.find_by_name(name)
-    self.all.detect{|song| song.name == name}
+    all.detect{|song| song.name == name}
   end
 
   def self.find_or_create_by_name(name)
