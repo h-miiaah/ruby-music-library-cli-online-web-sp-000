1df698546cf1ed094a7fe82ba5bca607da71a020
Haim D <haim11293@aol.com>
2019-11-13 16:40:34 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -47,4 +47,10 @@ class MusicLibraryController
     end
   end
 
+  def list_artists
+    artist = Artist.all.sort{|a,b| a.name <=> b.name}.each.with_index(1) do |artist, index|
+      puts "#{index}. #{artist.name}"
+    end
+  end
+
 end
