638f5ce8a35f7e2c19b8a6fdcb46f1e1c563ea40
Haim D <haim11293@aol.com>
2019-11-13 16:42:49 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -53,4 +53,10 @@ class MusicLibraryController
     end
   end
 
+  def list_genres
+    genres = Genre.all.sort{|a,b| a.name <=> b.name}.each.with_index(1) do |genre, index|
+      puts "#{index}. #{genre.name}"
+    end
+  end
+
 end
